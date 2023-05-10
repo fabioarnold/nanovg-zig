f9db8b0dfde5d85b477ac8e56c170dd20c391dac
Fabio Arnold <fabio@fabioarnold.de>
2023-05-10 22:34:35 +0200

    Update build.zig

--- build.zig
+++ build.zig
@@ -1,83 +1,84 @@
 const std = @import("std");
 
-fn getRootDir() []const u8 {
-    return std.fs.path.dirname(@src().file) orelse ".";
-}
-
-const root_dir = getRootDir();
-
-pub fn addCSourceFiles(artifact: *std.build.CompileStep) void {
-    artifact.addIncludePath(root_dir ++ "/src");
-    artifact.addCSourceFile(root_dir ++ "/src/fontstash.c", &.{ "-DFONS_NO_STDIO", "-fno-stack-protector" });
-    artifact.addCSourceFile(root_dir ++ "/src/stb_image.c", &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" });
-    artifact.linkLibC();
-}
-
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const target_wasm = if (target.cpu_arch) |arch| arch == .wasm32 or arch == .wasm64 else false;
-    const artifact = init: {
+    const nanovg = b.addModule("nanovg", .{ .source_file = .{ .path = "src/nanovg.zig" } });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "nanovg",
+        .root_source_file = .{ .path = "src/nanovg.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.addModule("nanovg", nanovg);
+    lib.addIncludePath("src");
+    lib.addCSourceFile("src/fontstash.c", &.{ "-DFONS_NO_STDIO", "-fno-stack-protector" });
+    lib.addCSourceFile("src/stb_image.c", &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" });
+    lib.linkLibC();
+
+    const target_wasm = if (target.cpu_arch) |arch| arch.isWasm() else false;
+    const demo = init: {
         if (target_wasm) {
             break :init b.addSharedLibrary(.{
-                .name = "main",
+                .name = "demo",
                 .root_source_file = .{ .path = "examples/example_wasm.zig" },
                 .target = target,
                 .optimize = optimize,
             });
         } else {
             break :init b.addExecutable(.{
-                .name = "main",
+                .name = "demo",
                 .root_source_file = .{ .path = "examples/example_glfw.zig" },
                 .target = target,
                 .optimize = optimize,
             });
         }
     };
-
-    const module = b.addModule("nanovg", .{ .source_file = .{ .path = root_dir ++ "/src/nanovg.zig" } });
-    artifact.addModule("nanovg", module);
-    addCSourceFiles(artifact);
+    demo.addModule("nanovg", nanovg);
+    demo.linkLibrary(lib);
+    b.installArtifact(lib);
 
     if (target_wasm) {
-        artifact.rdynamic = true;
+        demo.rdynamic = true;
     } else {
-        artifact.addIncludePath("lib/gl2/include");
-        artifact.addCSourceFile("lib/gl2/src/glad.c", &.{});
+        demo.addIncludePath("lib/gl2/include");
+        demo.addCSourceFile("lib/gl2/src/glad.c", &.{});
         if (target.isWindows()) {
-            artifact.addVcpkgPaths(.dynamic) catch @panic("vcpkg not installed");
-            if (artifact.vcpkg_bin_path) |bin_path| {
+            demo.addVcpkgPaths(.dynamic) catch @panic("vcpkg not installed");
+            if (demo.vcpkg_bin_path) |bin_path| {
                 for (&[_][]const u8{"glfw3.dll"}) |dll| {
                     const src_dll = try std.fs.path.join(b.allocator, &.{ bin_path, dll });
                     b.installBinFile(src_dll, dll);
                 }
             }
-            artifact.linkSystemLibrary("glfw3dll");
-            artifact.linkSystemLibrary("opengl32");
+            demo.linkSystemLibrary("glfw3dll");
+            demo.linkSystemLibrary("opengl32");
         } else if (target.isDarwin()) {
-            artifact.linkSystemLibrary("glfw3");
-            artifact.linkFramework("OpenGL");
+            demo.linkSystemLibrary("glfw3");
+            demo.linkFramework("OpenGL");
         } else if (target.isLinux()) {
-            artifact.linkSystemLibrary("glfw3");
-            artifact.linkSystemLibrary("GL");
-            artifact.linkSystemLibrary("X11");
+            demo.linkSystemLibrary("glfw3");
+            demo.linkSystemLibrary("GL");
+            demo.linkSystemLibrary("X11");
         } else {
             std.log.warn("Unsupported target: {}", .{target});
-            artifact.linkSystemLibrary("glfw3");
-            artifact.linkSystemLibrary("GL");
+            demo.linkSystemLibrary("glfw3");
+            demo.linkSystemLibrary("GL");
         }
     }
-    artifact.addIncludePath("examples");
-    artifact.addCSourceFile("examples/stb_image_write.c", &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" });
-    b.installArtifact(artifact);
+    demo.addIncludePath("src");
+    demo.addIncludePath("examples");
+    demo.addCSourceFile("examples/stb_image_write.c", &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" });
+    b.installArtifact(demo);
 
-    const run_cmd = b.addRunArtifact(artifact);
+    const run_cmd = b.addRunArtifact(demo);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("run", "Run the demo");
     run_step.dependOn(&run_cmd.step);
 }
