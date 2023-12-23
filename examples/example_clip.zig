3fea5b67718b3a84394da9e39f3e90d729b9a109
Fabio Arnold <fabio@fabioarnold.de>
2023-12-23 11:18:22 +0100

    Clean up paths

--- examples/example_clip.zig
+++ examples/example_clip.zig
@@ -15,19 +15,87 @@ fn keyCallback(window: ?*c.GLFWwindow, key: c_int, scancode: c_int, action: c_in
         c.glfwSetWindowShouldClose(window, c.GL_TRUE);
 }
 
-fn pathStar(vg: nvg, x: f32, y: f32, n: usize, inr: f32, outr: f32) void {
+fn scaleToFit(vg: nvg, w: f32, h: f32, target_w: f32, target_h: f32) void {
+    const sx = target_w / w;
+    const sy = target_h / h;
+    const s = @min(sx, sy);
+    vg.translate(0.5 * target_w, 0.5 * target_h);
+    vg.scale(s, s);
+    vg.translate(-0.5 * w, -0.5 * h);
+}
+
+fn pathStar(vg: nvg, n: usize, inr: f32, outr: f32) void {
     const to_angle = 2 * std.math.pi / @as(f32, @floatFromInt(n));
     for (0..n) |i| {
         const fi: f32 = @floatFromInt(i);
         const a0 = fi * to_angle;
         const a1 = (fi + 0.5) * to_angle;
         if (i == 0)
-            vg.moveTo(x + outr * @sin(a0), y - outr * @cos(a0))
+            vg.moveTo(outr * @sin(a0), -outr * @cos(a0))
         else
-            vg.lineTo(x + outr * @sin(a0), y - outr * @cos(a0));
-        vg.lineTo(x + inr * @sin(a1), y - inr * @cos(a1));
+            vg.lineTo(outr * @sin(a0), -outr * @cos(a0));
+        vg.lineTo(inr * @sin(a1), -inr * @cos(a1));
     }
     vg.closePath();
+}
+
+fn pathDonut(vg: nvg, r: f32) void {
+    vg.circle(0, 0, r);
+    vg.pathWinding(.cw);
+    vg.circle(0, 0, 0.4 * r);
+}
+
+fn pathHeart(vg: nvg, w: f32, h: f32) void {
+    vg.save();
+    defer vg.restore();
+
+    const min_x = 9;
+    const min_y = 11;
+    const max_x = 41;
+    const max_y = 38;
+    scaleToFit(vg, max_x - min_x, max_y - min_y, w, h);
+    vg.translate(-min_x, -min_y);
+    const heart = nvg.Path{
+        .verbs = &.{ .move, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier },
+        .points = &.{ 25, 38, 20, 34, 9, 27, 9, 19, 9, 14.5, 12.5, 11, 17, 11, 20, 11, 23, 12, 25, 15.5, 27, 12, 30, 11, 33, 11, 37.5, 11, 41, 14.5, 41, 19, 41, 27, 30, 34, 25, 38 },
+    };
+    vg.addPath(heart);
+}
+
+fn pathTwitterLogo(vg: nvg, w: f32, h: f32) void {
+    vg.save();
+    defer vg.restore();
+
+    scaleToFit(vg, 248, 204, w, h);
+
+    // const width = 248;
+    // const height = 204;
+    const twitter_logo = nvg.Path{
+        .verbs = &.{ .move, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier },
+        // zig fmt: off
+        .points = &.{
+            221.95,51.29,
+            222.1,53.46, 222.1,55.63, 222.1,57.82,
+            222.1,124.55, 171.3,201.51, 78.41,201.51,
+            50.97,201.51, 24.1,193.65, 1,178.83, 
+            4.99,179.31, 9,179.55, 13.02,179.56,
+            35.76,179.58, 57.85,171.95, 75.74,157.9,
+            54.13,157.49, 35.18,143.4, 28.56,122.83,
+            36.13,124.29, 43.93,123.99, 51.36,121.96,
+            27.8,117.2, 10.85,96.5, 10.85,72.46,
+            17.87,75.73, 25.73,77.9, 33.77,78.14,
+            11.58,63.31, 4.74,33.79, 18.14,10.71,
+            43.78,42.26, 81.61,61.44, 122.22,63.47,
+            118.15,45.93, 123.71,27.55, 136.83,15.22,
+            157.17,-3.9, 189.16,-2.92, 208.28,17.41,
+            219.59,15.18, 230.43,11.03, 240.35,5.15,
+            236.58,16.84, 228.69,26.77, 218.15,33.08,
+            228.16,31.9, 237.94,29.22, 247.15,25.13,
+            240.37,35.29 ,231.83,44.14, 221.95,51.29,
+        },
+        // zig fmt: on
+    };
+    vg.addPath(twitter_logo);
 }
 
 pub fn main() !void {
@@ -116,67 +184,44 @@ pub fn main() !void {
 
         vg.beginPath();
         vg.translate(100, 100);
-        vg.rotate(@floatCast(t));
-        pathStar(vg, 0, 0, 5, 35, 80);
+        const t32: f32 = @floatCast(t);
+        vg.rotate(t32);
+        pathStar(vg, 5, 35, 80);
+        vg.resetTransform();
         vg.fillColor(nvg.rgbaf(1, 1, 0, 0.5));
         vg.fill();
-        vg.resetTransform();
 
         // donut
         vg.beginPath();
-        vg.circle(250, 100, 50);
-        vg.pathWinding(.cw);
-        vg.circle(250, 100, 20);
+        vg.translate(250, 100);
+        pathDonut(vg, 50);
+        vg.resetTransform();
         vg.fillColor(nvg.rgbaf(1, 0.5, 0, 0.5));
         vg.fill();
 
         // heart
-        vg.translate(300, 0);
-        vg.scale(4, 4);
         vg.beginPath();
-        const half_heart = nvg.Path{
-            .verbs = &.{ .move, .bezier, .bezier, .bezier },
-            .points = &.{ 25, 38, 20, 34, 9, 27, 9, 19, 9, 14.5, 12.5, 11, 17, 11, 20, 11, 23, 12, 25, 15.5 },
-        };
-        vg.addPath(half_heart);
-        vg.translate(50, 0);
-        vg.scale(-1, 1);
-        vg.addPath(half_heart);
+        vg.translate(350, 50);
+        pathHeart(vg, 100, 100);
+        vg.resetTransform();
         vg.fillColor(nvg.rgbf(1, 0, 0));
         vg.fill();
-        vg.resetTransform();
 
-        const twitter = nvg.Path{
-            .verbs = &.{ .move, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier, .bezier },
-            // zig fmt: off
-            .points = &.{
-                221.95,51.29,
-                222.1,53.46, 222.1,55.63, 222.1,57.82,
-                222.1,124.55, 171.3,201.51, 78.41,201.51,
-                50.97,201.51, 24.1,193.65, 1,178.83, 
-                4.99,179.31, 9,179.55, 13.02,179.56,
-                35.76,179.58, 57.85,171.95, 75.74,157.9,
-                54.13,157.49, 35.18,143.4, 28.56,122.83,
-                36.13,124.29, 43.93,123.99, 51.36,121.96,
-                27.8,117.2, 10.85,96.5, 10.85,72.46,
-                17.87,75.73, 25.73,77.9, 33.77,78.14,
-                11.58,63.31, 4.74,33.79, 18.14,10.71,
-                43.78,42.26, 81.61,61.44, 122.22,63.47,
-                118.15,45.93, 123.71,27.55, 136.83,15.22,
-                157.17,-3.9, 189.16,-2.92, 208.28,17.41,
-                219.59,15.18, 230.43,11.03, 240.35,5.15,
-                236.58,16.84, 228.69,26.77, 218.15,33.08,
-                228.16,31.9, 237.94,29.22, 247.15,25.13,
-                240.37,35.29 ,231.83,44.14, 221.95,51.29,
-            },
-            // zig fmt: on
-        };
-        vg.translate(500, 50);
-        vg.scale(0.5, 0.5);
+        // twitter logo
         vg.beginPath();
-        vg.addPath(twitter);
+        vg.translate(500, 50);
+        pathTwitterLogo(vg, 100, 100);
+        vg.resetTransform();
         vg.fillColor(nvg.rgb(0x1D, 0x9B, 0xF0));
         vg.fill();
+
+        vg.beginPath();
+        vg.translate(200, 200);
+        pathTwitterLogo(vg, 200, 200);
+        vg.translate(100, 100);
+        pathDonut(vg, 100);
+        vg.strokeColor(nvg.rgb(0, 0, 0));
+        vg.stroke();
 
         vg.endFrame();
 
