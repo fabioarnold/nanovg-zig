0f132c1d6489a5e7b1528345105b1ee44d537a21
Fabio Arnold <fabio@fabioarnold.de>
2024-08-01 21:11:29 +0200

    Update to Zig master

--- build.zig
+++ build.zig
@@ -5,20 +5,22 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const nanovg_mod = b.addModule("nanovg", .{
-        .root_source_file = .{ .path = "src/nanovg.zig" },
+        .root_source_file = b.path("src/nanovg.zig"),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
-    nanovg_mod.addIncludePath(.{ .path = "src" });
-    nanovg_mod.addIncludePath(.{ .path = "lib/gl2/include" });
-    nanovg_mod.addCSourceFile(.{ .file = .{ .path = "src/fontstash.c" }, .flags = &.{ "-DFONS_NO_STDIO", "-fno-stack-protector" } });
-    nanovg_mod.addCSourceFile(.{ .file = .{ .path = "src/stb_image.c" }, .flags = &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" } });
+    nanovg_mod.addIncludePath(b.path("src"));
+    nanovg_mod.addIncludePath(b.path("lib/gl2/include"));
+    nanovg_mod.addCSourceFile(.{ .file = b.path("src/fontstash.c"), .flags = &.{ "-DFONS_NO_STDIO", "-fno-stack-protector" } });
+    nanovg_mod.addCSourceFile(.{ .file = b.path("src/stb_image.c"), .flags = &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" } });
 
     if (target.result.isWasm()) {
         _ = installDemo(b, target, optimize, "demo", "examples/example_wasm.zig", nanovg_mod);
     } else {
-        _ = installDemo(b, target, optimize, "demo_glfw", "examples/example_glfw.zig", nanovg_mod);
+        const demo_glfw = installDemo(b, target, optimize, "demo_glfw", "examples/example_glfw.zig", nanovg_mod);
+        demo_glfw.addIncludePath(b.path("examples"));
+        demo_glfw.addCSourceFile(.{ .file = b.path("examples/stb_image_write.c"), .flags = &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" } });
         _ = installDemo(b, target, optimize, "demo_fbo", "examples/example_fbo.zig", nanovg_mod);
         _ = installDemo(b, target, optimize, "demo_clip", "examples/example_clip.zig", nanovg_mod);
     }
@@ -27,21 +29,18 @@ pub fn build(b: *std.Build) !void {
 fn installDemo(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, name: []const u8, root_source_file: []const u8, nanovg_mod: *std.Build.Module) *std.Build.Step.Compile {
     const demo = b.addExecutable(.{
         .name = name,
-        .root_source_file = .{ .path = root_source_file },
+        .root_source_file = b.path(root_source_file),
         .target = target,
         .optimize = optimize,
     });
     demo.root_module.addImport("nanovg", nanovg_mod);
 
-    demo.addIncludePath(.{ .path = "examples" });
-    demo.addCSourceFile(.{ .file = .{ .path = "examples/stb_image_write.c" }, .flags = &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" } });
-
     if (target.result.isWasm()) {
         demo.rdynamic = true;
         demo.entry = .disabled;
     } else {
-        demo.addIncludePath(.{ .path = "lib/gl2/include" });
-        demo.addCSourceFile(.{ .file = .{ .path = "lib/gl2/src/glad.c" }, .flags = &.{} });
+        demo.addIncludePath(b.path("lib/gl2/include"));
+        demo.addCSourceFile(.{ .file = b.path("lib/gl2/src/glad.c"), .flags = &.{} });
         switch (target.result.os.tag) {
             .windows => {
                 b.installBinFile("glfw3.dll", "glfw3.dll");
@@ -49,8 +48,8 @@ fn installDemo(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.bu
                 demo.linkSystemLibrary("opengl32");
             },
             .macos => {
-                demo.addIncludePath(.{ .path = "/opt/homebrew/include" });
-                demo.addLibraryPath(.{ .path = "/opt/homebrew/lib" });
+                demo.addIncludePath(b.path("/opt/homebrew/include"));
+                demo.addLibraryPath(b.path("/opt/homebrew/lib"));
                 demo.linkSystemLibrary("glfw");
                 demo.linkFramework("OpenGL");
             },
